a94aaed9ba94a7f96a31ec6ccfa5144295e803f5
Vittorio Fusco <fuscovittorio89@gmail.com>
2023-10-12 17:14:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const tabella = document.getElementById("tabella");
+
+for (let i = 0; i < 77; i++) {
+  document.createElement("tabella");
+  const tabella = [i];
+  console.log(tabella);
+}
+
+/*
+$(function valori() {
+  let allNumber = [];
+  let numeriSelezionati = [];
+
+  for (let i = 0; i < 76; i++) {
+    $("#tabella").append(`<span class="extract-number"> ${i} </span>`);
+    allNumber.push(i);
+  }
+});
+*/
